0f72f9dec673aef8d4563381c80e68cd8f3e10a4
João Pedro Fagundes <70774421+AcessDnd@users.noreply.github.com>
2022-05-01 20:57:58 -0300

    Add files via upload

---
+++ quick sort/livros.js
@@ -0,0 +1,48 @@
+const livros = [
+    {
+        titulo: 'JS',
+        valor: 20
+    },
+    {
+        titulo: 'CSS',
+        valor: 40
+    },
+    {
+        titulo: 'html',
+        valor: 80
+    },
+    {
+        titulo: 'json',
+        valor: 103.50
+    },
+    {
+        titulo: 'JAVA',
+        valor: 13.21
+    },
+    {
+        titulo: 'Node',
+        valor: 20
+    },
+    {
+        titulo: 'Elixir',
+        valor: 80
+    },
+    {
+        titulo: 'Canvas',
+        valor: 90
+    },
+    {
+        titulo: 'Webkit',
+        valor: 105.70
+    },
+    {
+        titulo: 'Mozilla',
+        valor: 230
+    },
+    {
+        titulo: 'Chrome',
+        valor: 600
+    }
+]
+
+module.exports = livros;
